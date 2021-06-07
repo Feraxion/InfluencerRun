7dfaaa786c4d385617308ec0429d4ae520b21e59
aybarskesebir <56149031+aybarskesebir@users.noreply.github.com>
2021-06-07 15:58:20 +0300

    test

---
+++ Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class GameSceneManager {
+
+    public enum Scene {
+        Level1,
+        Level2,
+        Level3,
+    }
+
+    public static void Load(Scene scene) {
+        SceneManager.LoadScene(scene.ToString());
+    }
+
+}
